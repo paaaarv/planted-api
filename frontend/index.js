6028db2b4e0ec1ee07e80c578081c5c67dcfedd4
paaaarv <pkumar@trinity.edu>
2021-06-08 21:17:49 -0500

    creating form with JS

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,25 @@
 
+const addPlant = document.getElementById("addPlant");
+const body = document.getElementById("body");
+addPlant.addEventListener('click', function(){
+    const form= new Form
+    let label;
+    let input;
+    body.innerHTML+="<form>"
+    for(let i=0; i< form.attributes.length; i++){
+        label=document.createElement("label")
+        label.setAttribute("for", `${form.attributes[i]}`)
+        label.innerHTML=`${form.attributes[i]}`
+        input = document.createElement("input")
+        input.setAttribute("type", "text")
+        input.setAttribute("value", `${form.attributes[i]}`)
+        body.appendChild(label)
+        body.appendChild(input)
+
+    }
+    body.innerHTML+="</form>"
+})
+
 class Form {
     constructor(){
         this.attributes=['name', 'light', 'water', 'fertilizer', 'notes']
